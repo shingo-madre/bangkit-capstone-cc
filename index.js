6d62cd8822efd5be8c7dff5a1a3d83ca95ed9754
shingo-madre <rakazein.akbar@gmail.com>
2022-05-31 19:34:16 +0700

    new routes for crop data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const config = require('./config');
 const userRoutes = require('./routes/user-routes');
+const cropDataRoutes = require('./routes/cropData-routes');
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', userRoutes.routes);
+app.use('/data', cropDataRoutes.routes);
 
 app.listen(config.port, () => console.log('API is running on http://localhost:' + config.port));
