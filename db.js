ea440e75d83a51c027314e37f9e5b94cc0b2c801
shingo-madre <rakazein.akbar@gmail.com>
2022-06-04 12:18:47 +0700

    Removing *ask for details*

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const Firestore = require('@google-cloud/firestore');
 
 const db = new Firestore({
   projectId: 'protean-torus-344012',
-  keyFilename: '\protean-torus-344012-36c039628269.json',
+  keyFilename: '../protean-torus-344012-36c039628269.json',
 });
 
 module.exports = db;
