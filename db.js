064762143f9d955fab70934e02a4b52972587516
shingo-madre <rakazein.akbar@gmail.com>
2022-06-09 19:39:14 +0700

    new service acc token

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const Firestore = require('@google-cloud/firestore');
 
 const db = new Firestore({
   projectId: 'protean-torus-344012',
-  keyFilename: '../protean-torus-344012-36c039628269.json',
+  keyFilename: './protean-torus-344012-3d77ed15505c.json',
 });
 
 module.exports = db;
